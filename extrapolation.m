3b9e66c9e93264196d5e95daa9f96c49093ffa92
yanghanwzl <1725084630@qq.com>
2026-05-05 10:07:20 +0800

    Update extrapolation.m

--- extrapolation.m
+++ extrapolation.m
@@ -17,7 +17,7 @@ for j = 1:2
                   accumarray(Tb(3, 1:2*NE(1)*NE(2))',sum(NL_2(:,19:27),2), size(FF));
         K_v = K1/2 + M/(tau) + M/2;
         F_v = Fv_1 + M*(v(1,:).'/(tau)) + FF_v - K1*v(1,:).'/2 - M*v(1,:).'/2;
-        [K_v,F_v] = bon(K_v,F_v,NE,Pb,tc,BC,nu_v_x,mu_v_x,nu_v_y,mu_v_y,1);
+        [K_v,F_v] = bou(K_v,F_v,NE,Pb,tc,BC,nu_v_x,mu_v_x,nu_v_y,mu_v_y,1);
         v(2,:) = K_v\F_v;
     else
         for i = 1:9
@@ -46,7 +46,7 @@ for j = 1:2
         K_u = K_u/2;
         K = K1/2 + 1i*M/(tau) + K_u;
         F = F + 1i*M*(w(1,:).'/(tau)) - FF - K1*w(1,:).'/2;
-        [K,F] = bon(K,F,NE,Pb,tc,BC,nu_x,mu_x,nu_y,mu_y,1);
+        [K,F] = bou(K,F,NE,Pb,tc,BC,nu_x,mu_x,nu_y,mu_y,1);
         w(2,:) = K\F;
 
         for i = 1:9   
@@ -60,7 +60,7 @@ for j = 1:2
                   accumarray(Tb(3, 1:2*NE(1)*NE(2))',sum(NL_2(:,19:27),2), size(FF));
         K_v = K1/2 + M/(tau) + M/2;
         F_v = Fv_1 + M*(v(1,:).'/(tau)) + FF_v - K1*v(1,:).'/2 - M*v(1,:).'/2;
-        [K_v,F_v] = bon(K_v,F_v,NE,Pb,tc,BC,nu_v_x,mu_v_x,nu_v_y,mu_v_y,1);
+        [K_v,F_v] = bou(K_v,F_v,NE,Pb,tc,BC,nu_v_x,mu_v_x,nu_v_y,mu_v_y,1);
         v(2,:) = K_v\F_v;
     end   
 end
